4e65068912ba994e02a2aeaa3fa673298dfcdd21
nghiadaotda <nghia.dao@tda.company>
2022-04-19 16:28:37 +0700

    feat:create updateTask function

--- src/views/UpdateTask.vue
+++ src/views/UpdateTask.vue
@@ -1,43 +1,47 @@
 <template>
-    <form>
-        <div>
-            <label for="">Task name: </label>
-            <input type="text" v-model="taskUpdate.taskName">
-        </div>
-        <div>
-            <label for="">Person: </label>
-            <input type="text" v-model="taskUpdate.taskPerson">
-        </div>
-        <div>
-            <label for="">Duration(Day): </label>
-            <input type="number" v-model="taskUpdate.duration">
-        </div>
-        <!-- <div>
-            <label>Status: </label>
-            <select name="" id="" v-model="taskUpdate.status">
-                <option value="Inprogress">Inprogress</option>
-                <option value="Done">Done</option>
-                <option value="Cancel">Cancel</option>
-            </select>
-        </div> -->
-        <button @click="updateTask">Update</button>
-    </form>
+  <form>
+    <div>
+      <label for="">Task name: </label>
+      <input type="text" v-model="taskUpdate.taskName" />
+    </div>
+    <div>
+      <label for="">Person: </label>
+      <input type="text" v-model="taskUpdate.taskPerson" />
+    </div>
+    <div>
+      <label for="">Duration(Day): </label>
+      <input type="number" v-model="taskUpdate.duration" />
+    </div>
+    <div>
+      <label>Status: </label>
+      <select name="" id="" v-model="taskUpdate.status">
+        <option value="Inprogress">Inprogress</option>
+        <option value="Done">Done</option>
+        <option value="Cancel">Cancel</option>
+      </select>
+    </div>
+    <button @click="updateTask">Update</button>
+  </form>
 </template>
 
 <script>
 export default {
-    name: 'UpdateTask',
-    method: {
-        updateTask(e) {
-            e.preventDefault();
-            console.log('hello')
-            this.$store.commit('updateTask', this.taskUpdate)
-        },
+  name: "UpdateTask",
+  methods: {
+    updateTask(e) {
+      e.preventDefault();
+      this.$store.commit('updateTask', this.taskUpdate)
+      this.$router.push({path: '/'})
     },
-    computed: {
-        taskUpdate() {
-            return {...this.$store.state.tasks.find(task => task.taskId == this.$route.params.taskId)}
-        }
-    }
-}
+  },
+  computed: {
+    taskUpdate() {
+      return {
+        ...this.$store.state.tasks.find(
+          (task) => task.taskId == this.$route.params.taskId
+        ),
+      };
+    },
+  },
+};
 </script>
